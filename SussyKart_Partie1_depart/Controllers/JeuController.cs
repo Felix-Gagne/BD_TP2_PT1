e069a8c843db56428b1daf9f5bdde9e6f3202f58
Félix Gagné <2070094@cegepmontpetit.ca>
2023-04-24 17:12:28 -0400

    Statistique de course (WiP)

--- SussyKart_Partie1_depart/Controllers/JeuController.cs
+++ SussyKart_Partie1_depart/Controllers/JeuController.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SussyKart_Partie1.Data;
+using SussyKart_Partie1.Models;
 using SussyKart_Partie1.ViewModels;
+using System.Security.Claims;
+using System.Security.Principal;
 
 namespace SussyKart_Partie1.Controllers
 {
     public class JeuController : Controller
     {
+        readonly TP2_SussyKartContext _context;
+
+        public JeuController(TP2_SussyKartContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -20,10 +34,43 @@ namespace SussyKart_Partie1.Controllers
             return View();
         }
 
+        [Authorize]
         [HttpPost]
-        public IActionResult Jouer(ParticipationVM pvm)
+        public async Task<IActionResult> Jouer(ParticipationVM pvm)
         {
             // Le paramètre pvm est déjà rempli par la View Jouer et il est reçu par cette action... qui est vide.
+
+            string pseudo = HttpContext.User.FindFirstValue(ClaimTypes.Name);
+            Utilisateur? user = await _context.Utilisateurs.FirstOrDefaultAsync(x => x.Pseudo == pseudo);
+            if(user == null)
+            {
+                ModelState.AddModelError("Mauvaise authorization", "Veuillez vous connecter avant");
+                return View(pvm);
+            }
+            else
+            {
+                string query = "EXEC Courses.USP_InsertParticipation @Position, @Chrono, @NbJoueur, @NomCourse, @IdUser";
+                List<SqlParameter> parameters = new List<SqlParameter>
+                {
+                    new SqlParameter{ParameterName = "@Position", Value = pvm.Position},
+                    new SqlParameter{ParameterName = "@Chrono", Value = pvm.Chrono},
+                    new SqlParameter{ParameterName = "@NbJoueur", Value = pvm.NbJoueurs},
+                    new SqlParameter{ParameterName = "@NomCourse", Value = pvm.NomCourse},
+                    new SqlParameter{ParameterName = "@IdUser", Value = user.UtilisateurId},
+                };
+
+                try
+                {
+                    await _context.Database.ExecuteSqlRawAsync(query, parameters.ToArray());
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Une erreur est survenue. Veuillez réessayez plus tard.");
+                    return View(pvm);
+                }
+            }
+
+
             return View();
         }
     }
---
+++ SussyKart_Partie1_depart/Sql_Scripts/V2.3__InsertParticipation.sql
@@ -0,0 +1,24 @@
+﻿IF EXISTS(SELECT * FROM sys.procedures WHERE name='USP_InsertParticipation')
+BEGIN
+	DROP PROCEDURE Utilisateurs.USP_CreerUtilisateur;
+END
+GO
+
+CREATE PROCEDURE Courses.USP_InsertParticipation
+	@Position int,
+	@Chrono int,
+	@NbJoueur int,
+	@NomCourse nvarchar(50), 
+	@IdUser int
+AS
+BEGIN
+	DECLARE @IdCourse int;
+
+	SELECT @IdCourse=CourseID FROM Courses.Course
+	WHERE @NomCourse=Nom
+
+	INSERT INTO Courses.ParticipationCourse (Position, Chrono, NbJoueurs, DateParticipation, CourseID, UtilisateurID)
+	VALUES(@Position, @Chrono, @NbJoueur, GETDATE(), @IdCourse, @IdUser);
+END
+GO
+
