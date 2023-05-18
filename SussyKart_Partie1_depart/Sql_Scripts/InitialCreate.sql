4e951b6bd718ffe172cd14ef53f9f7c7f6852558
Félix Gagné <2070094@cegepmontpetit.ca>
2023-05-18 08:53:21 -0400

    (WiP)

--- SussyKart_Partie1_depart/Sql_Scripts/InitialCreate.sql
+++ SussyKart_Partie1_depart/Sql_Scripts/InitialCreate.sql
@@ -10,3 +10,16 @@ BEGIN
     DROP DATABASE TP2_SussyKart
 END
 CREATE DATABASE TP2_SussyKart
+
+EXEC sp_configure filestream_access_level, 2 RECONFIGURE
+
+ALTER DATABASE TP2_SussyKart
+ADD FILEGROUP FG_Images CONTAINS FILESTREAM;
+GO
+ALTER DATABASE TP2_SussyKart
+ADD FILE (
+	NAME = FG_Images,
+	FILENAME = 'C:\EspaceLabo\FG_Images'
+)
+TO FILEGROUP FG_Images
+GO
